1c561f4043d9d03d8df685d8a30abeba872c4603
wurunpeng <fengmozhui@163.com>
2014-12-28 22:50:07 +0000

    修改lib/string.asm文件，添加strchr strrchr strcspn函数

--- include/string.h
+++ include/string.h
@@ -1,4 +1,6 @@
+extern char * strcpy(char * dest,const char *src);
 
+extern char * strncpy(char * dest,const char *src,int count);
 
 extern char * strcat(char * dest,const char * src);
 
@@ -8,3 +10,10 @@ extern char * strncat(char * dest,const char * src,int count);
 extern int strcmp(const char * cs,const char * ct);
 
 extern int strncmp(const char * cs,const char * ct,int count);
+
+extern char * strchr(const char * s,char c);
+
+
+extern char * strrchr(const char * s,char c);
+
+extern int strcspn(const char * cs, const char * ct);
--- lib/string.asm
+++ lib/string.asm
@@ -2,7 +2,67 @@
 
 ;  rdi,	rsi, rdx, rcx
 section	.text
+;--------------------------------------------------
+global 	strcpy
 
+strcpy:
+
+	cld
+
+	push 	rbp
+	mov 	rbp, 	rsp
+
+	sub 	rsp, 	8 * 4	; 	reserve 32B space 
+
+	push	rdi 			;	store the pointer
+
+.1:
+	lodsb
+	stosb
+
+	test 	al, 	al
+	jne 	.1
+
+	pop 	rax				; 	get the pointer
+
+	leave
+	ret
+
+;--------------------------------------------------
+global 	strncpy
+
+strncpy:
+
+	cld
+
+	push 	rbp
+	mov 	rbp, 	rsp
+
+	sub 	rsp, 	8 * 4	; 	reserve 32B space 
+
+	mov 	rcx, 	rdx 	; 	3rd argument
+
+	push	rdi 			;	store the pointer
+
+.1:
+	dec 	rcx 
+	js 		.2
+	lodsb
+	stosb
+
+	test 	al, 	al
+	jne 	.1
+
+	rep
+	stosb
+
+.2:
+	pop 	rax				; 	get the pointer
+
+	leave
+	ret
+
+;--------------------------------------------------
 global strcat
 
 strcat:
@@ -106,7 +166,7 @@ strcmp:
 	leave
 	ret
 
-; ;--------------------------------------------------
+;--------------------------------------------------
 global 	strncmp
 
 strncmp:
@@ -138,5 +198,120 @@ strncmp:
 
 
 .3:
+	leave
+	ret
+
+;--------------------------------------------------
+global 	strchr 
+
+strchr:
+
+	cld
+
+	push 	rbp
+	mov 	rbp, 	rsp
+
+	mov 	rax, 	rsi 
+	xor 	ah, 	ah
+	mov 	ah, 	al 		;	char --> ah
+
+	mov 	rsi, 	rdi 
+.1:
+	lodsb
+
+	cmp 	ah, 	al 		
+	je 		.2
+	test 	al, 	al
+	jne		.1
+
+	mov 	rsi, 	1 		;  return rsi - 1 = NULL(0)
+
+.2:
+	dec 	rsi 
+
+	mov 	rax, 	rsi
+.3:
+	leave
+	ret
+
+
+
+;--------------------------------------------------
+global 	strrchr 
+
+strrchr:
+
+	cld
+
+	push 	rbp
+	mov 	rbp, 	rsp
+
+	mov 	rax, 	rsi 
+	xor 	ah, 	ah
+	mov 	ah, 	al 		;	char --> ah
+
+	mov 	rsi, 	rdi 	; 	rsi = string address
+	xor 	rdx, 	rdx 	; 	rdx = 0
+.1:
+	lodsb
+
+	cmp 	ah, 	al 		
+	jne		.2
+	mov 	rdx, 	rsi
+	dec 	rdx 			; 	rdx = rsi - 1
+.2:
+	test 	al, 	al
+	jne		.1
+
+	mov 	rax, 	rdx 
+
+	leave
+	ret
+
+;--------------------------------------------------
+global 	strcspn
+
+strcspn:
+
+	cld
+
+	push 	rbp
+	mov 	rbp, 	rsp
+
+	sub 	rsp,	4 * 8	
+
+	xchg 	rdi, 	rsi 	; rsi(1st arg), rdi(2nd arg)
+	mov 	[rsp], 	rsi 
+	mov 	[rsp + 8], 	rdi 
+
+	mov 	al, 	0
+	mov 	rcx, 	0xffffffffffffffff
+	repne 	
+	scasb
+	dec 	rdi 
+	mov 	rdx, 	rdi
+	sub 	rdx, 	[rsp + 8]
+	mov 	[rsp + 16], 	rdx 	; the length of 2nd arg
+
+.1:
+	mov 	rdi, 	[rsp + 8]
+	lodsb
+
+	test 	al, 	al
+	je 		.2
+
+	mov 	rcx, 	[rsp + 16]
+	repne 
+	scasb 
+	jne 	.1
+
+.2:
+	dec 	rsi
+	mov 	rdx, 	rsi
+	sub 	rdx, 	[rsp]
+	mov 	rax, 	rdx
+
+	; mov 	rax, 	[rsp + 16]
+	
 	leave
 	ret
